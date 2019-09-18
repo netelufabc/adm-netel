6acba4538ffe923ff894a770a79c711cd6a7b0e0
gustavo <gustavo.castilho@ufabc.edu.br>
2019-09-18 11:02:35 -0300

    Alterado método de login para permitir escolher o papél quando o usuário está cadastrado em múltiplos papéis.

--- application/models/Model_login.php
+++ application/models/Model_login.php
@@ -6,13 +6,15 @@ class Model_login extends CI_Model {
 
     public function Get_user_and_role($login) {
 
-        $this->db->select('user.id,user.login,user_role.role_id as role, user_role.project_id');
-        $this->db->from('user');
-        $this->db->join('user_role', 'user.id = user_role.user_id', 'left');
+        $this->db->select('user_role.role_id, user.id, user.login, user.email, user.name, roles.title, roles.description');
+        $this->db->distinct();
+        $this->db->from('user_role');
+        $this->db->join('user', 'user.id = user_role.user_id', 'left');
+        $this->db->join('roles' , 'user_role.role_id = roles.id');
         $this->db->where('user.login', $login);
         $query = $this->db->get();
         if ($query->num_rows() == 0) {
-            $this->session->set_flashdata('invalid_credentials', 'Login não cadastrado no sistema!');
+            $this->session->set_flashdata('invalid_credentials', 'Login não cadastrado ou sem papel definido!');
             redirect('Ctrl_main');
         } else {
             return $query->result();
